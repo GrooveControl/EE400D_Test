cea45fbf187b546fc5606a9b7cc8ce8017b5bc78
Alan <alanvalles@gmail.com>
2016-09-15 13:29:13 -0700

    additional comments

--- GitTest.ino
+++ GitTest.ino
@@ -1,9 +1,20 @@
+//--------------------------\\
+// Title Block
+// Addtnl features will be added in this stage
+// contact alanvallesgmail.com
+//
+//--------------------------\\
 void setup() {
   // put your setup code here, to run once:
-
+// initialize other!!
+//Hello f
+//
+int var = 8 ;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  digitalWrite(13,HiGH);
 
 }
